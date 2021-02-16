e19be6e80e3540af4325de6d5b174612304a6bc8
Allen <jihoshin.28@gmail.com>
2021-02-15 17:35:20 -0800

    added models

---
+++ app/server/models/todo_item.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field
+
+class TodoItemSchema(BaseModel):
+    item: str = Field(...)
+    title: str = Field(...)
+
+class UpdateItemMode(BaseModel):
+    item: Optional[str]
+    title: Optional[str]
+
+
+def ResponseModel(data, message):
+    return{
+        "data": data,
+        "code": 200,
+        "message": message
+    }
+
+def ErrorResponseModel(error, code, message):
+    return{"error", "code": code, "message": message}
---
+++ app/server/models/user.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field
+
+class UserSchema(BaseModel):
+    firstname: str = Field(...)
+    lastname: str = Field(...)
+    email: EmailStr = Field(...)
+    
+class UpdateUserModel(BaseModel):
+    firstname: Optional[str]
+    lastname: Optional[str]
+    email: Option[EmailStr]
+
+def ResponseModel(data, message):
+    return{
+        "data": data,
+        "code": 200,
+        "message": message
+    }
+
+def ErrorResponseModel(error, code, message):
+    return{"error", "code": code, "message": message}
